20724a1b7c6dea48d8b46d9f9ede06f0e39a83d8
username25461 <57962971+username25461@users.noreply.github.com>
2021-08-13 00:24:26 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'kclick_client.php';
+$client = new KClickClient('https://luxepriz.tk/api.php', 'y5th3pj75ckhvt74hxdwmph9px9zqf7t');
+$client->sendAllParams();       // to send all params from page query
+$client->params('');  // send params like param1=c&param2=b
+// $client->sendUtmLabels();     // to send only utm labels
+// $client->debug();              // to enable debug mode and show the errors
+// $client->currentPageAsReferrer(); // to send current page as referrer value
+// $client->keyword('KEYWORD');  // send custom keyword
+// $client->param('extra_param_5', '123'); // you can send any params
+$client->execute();             // request to api, show the output and continue
+// $client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
